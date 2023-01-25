16339b9e36991c5b80ab98ccae141ea971c7350a
Laurent Jouron <jouronlaurent@hotmail.com>
2023-01-25 12:00:24 +0100

    commit refactore requirements.txt 2022-01-25 half-day

--- player/controller.py
+++ player/controller.py
@@ -43,7 +43,8 @@ class Player:
 
         first_name = self.view.get_first_name()
         last_name = self.view.get_last_name()
-        player = Model(first_name=first_name, last_name=last_name)
+        player = Model(first_name=first_name,
+                       last_name=last_name)
         player.remove()
         self.view.remove_confirmation(first_name, last_name)
 
@@ -51,11 +52,17 @@ class Player:
         """Get a player with his first-name"""
         first_name = self.view.get_first_name()
         last_name = self.view.get_last_name()
-        player = Model.get_one_player(first_name=first_name, last_name=last_name)
-        return player["first_name"], player["last_name"], player["birthday"], player["gender"], player["ranking"]
+        player = Model.get_one_player(first_name=first_name,
+                                      last_name=last_name)
+        return player["first_name"], \
+            player["last_name"], \
+            player["birthday"], \
+            player["gender"], \
+            player["ranking"]
 
     def remove_player_list(self):
         """Removes an item from the list"""
         first_name = self.view.get_first_name()
         last_name = self.view.get_last_name()
-        return Model(first_name=first_name, last_name=last_name)
+        return Model(first_name=first_name,
+                     last_name=last_name)
